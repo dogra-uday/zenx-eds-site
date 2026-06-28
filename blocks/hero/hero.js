29387b3e77b2e7e2f3a474f43099b399e82bb84f
Dogra, Uday <uday.dogra+abcam@abcam.com>
2026-06-28 21:46:20 +0530

    Hero Changes2

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,9 +1,5 @@
 export default async function decorate(block) {
   const img = block.querySelector('img');
-  const text = block.querySelector(':scope > div:nth-child(2)');
-  const title = text ? text.querySelector('h1, h2, h3, h4, h5, h6') : null;
-  const paragraph = text ? text.querySelector('p') : null;
-  const link = text ? text.querySelector('a') : null;
 
   block.innerHTML = '';
 
@@ -12,12 +8,4 @@ export default async function decorate(block) {
     img.style.display = 'block';
     block.appendChild(img);
   }
-
-  const content = document.createElement('div');
-
-  if (title) content.appendChild(title);
-  if (paragraph) content.appendChild(paragraph);
-  if (link) content.appendChild(link);
-
-  block.appendChild(content);
 }
