c70072aa6188e667165c8690c4b33600c50665a6
Dogra, Uday <uday.dogra+abcam@abcam.com>
2026-06-28 21:17:20 +0530

    fix hero block HTML

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,15 +1,3 @@
-import { getHero } from '../../scripts/aem.js';
-
 export default async function decorate(block) {
-  const hero = await getHero();
-
-  const cta = hero.ctaLink
-    ? `<a href="${hero.ctaLink}">${hero.ctaText || ''}</a>`
-    : '';
-
-  block.innerHTML = `
-    <h1>${hero.title || ''}</h1>
-    <p>${hero.subtitle || ''}</p>
-    ${cta}
-  `;
+  block.innerHTML = '<h1>The Indian Bank</h1>';
 }
