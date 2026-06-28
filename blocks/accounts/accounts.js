bdc6308c9c1b93f84fa619bfecea11f6be54f38d
Dogra, Uday <uday.dogra+abcam@abcam.com>
2026-06-28 10:54:07 +0530

    fix accounts lint errors

--- blocks/accounts/accounts.js
+++ blocks/accounts/accounts.js
@@ -3,13 +3,12 @@ import { getAccounts } from '../../scripts/aem.js';
 export default async function decorate(block) {
   const data = await getAccounts();
 
-  block.innerHTML = data.map((a) =>
-    '<div>' +
-    '<h3>' + a.title + '</h3>' +
-    '<p>' + a.description + '</p>' +
-    '<div>' + a.interestRate + '</div>' +
-    '<small>' + a.minBalance + '</small>' +
-    '</div>'
-  ).join('');
+  block.innerHTML = data.map((a) => `
+    <div class="card">
+      <h3>${a.title}</h3>
+      <p>${a.description}</p>
+      <div>${a.interestRate}</div>
+      <small>${a.minBalance}</small>
+    </div>
+  `).join('');
 }
-``
