fbbdfea5550cd5e26c7581bf53c0ed32890023cc
Dogra, Uday <uday.dogra+abcam@abcam.com>
2026-06-29 10:11:03 +0530

    Make hero authorable finally pick dam 4

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,15 +1,7 @@
 export default async function decorate(block) {
-  const picture = block.querySelector('picture');
-
-  block.innerHTML = '';
-
-  if (picture) {
-    picture.style.width = '100%';
-    block.appendChild(picture);
-  }
-
-  const title = document.createElement('h1');
-  title.textContent = 'The Indian Bank';
-
-  block.appendChild(title);
+  block.innerHTML = `
+    <picture>
+      <img src="/content/dam/zenx-eds-site/Hero.png" style="width:100%;" />
+    </picture>
+  `;
 }
